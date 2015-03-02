dddbe1d2c0a3738fb086a91705300cbb5a4a9bc9
joecacti2 <joecacti@yahoo.com>
2015-03-02 09:51:40 -0500

    update for powershell and file names
    I updated to remove the dependency on wget, changed the method to grab the current IP from the text file and changed the filenames to be more representative of their functions.

--- DynIPToNote.bat
+++ DynIPToNote.bat
@@ -4,27 +4,21 @@ REM Fred Miller - joecacti@yahoo.com
 REM This batch file grabs your IP address and writes it to Evernote.
 REM It creates a new note each time it gets a new IP
 REM You can leave this running on your home computer and your home IP will always be known to you!
-REM Dependencies:  Evernote and GnuWin32 wget (http://gnuwin32.sourceforge.net/packages/wget.htm)
+REM Dependencies:  Evernote and powershell
 
-"C:\Program Files (x86)\GnuWin32\bin\wget.exe" -Oc:\zzz.txt http://bot.whatismyipaddress.com/
+REM No need for wget anymore "C:\Program Files (x86)\GnuWin32\bin\wget.exe" -Oc:\zzz.txt http://bot.whatismyipaddress.com/
 
-"C:\Program Files (x86)\Evernote\Evernote\Enscript.exe" exportNotes /qDynIPAddress /fc:\xxx.txt
+powershell -command (new-object System.Net.WebClient).DownloadFile('http://bot.whatismyipaddress.com/','C:\currentIP.txt')
 
-setlocal ENABLEDELAYEDEXPANSION
-set vidx=0
-for /F "tokens=*" %%A in (c:\zzz.txt) do (
-    SET /A vidx=!vidx! + 1
-    set var!vidx!=%%A
-)
-set var
+"C:\Program Files (x86)\Evernote\Evernote\Enscript.exe" exportNotes /qDynIPAddress /fc:\oldIPs.txt
 
-findstr /m %var1% c:\xxx.txt
+set /p var1=<c:\currentIP.txt
+
+findstr /m %var1% c:\oldIPs.txt
 
 if %errorlevel%==0 (
 echo Found current IP Address in Evernote DynIPAddress, will not do anything
 ) else (
-"C:\Program Files (x86)\Evernote\Evernote\Enscript.exe" createNote /s "c:\zzz.txt" /n "IPs" /i "DynIPAddress" 
+"C:\Program Files (x86)\Evernote\Evernote\Enscript.exe" createNote /s "c:\currentIP.txt" /n "IPs" /i "DynIPAddress" 
 echo New IP Address found, will create new Note, yay
 )
-
-
